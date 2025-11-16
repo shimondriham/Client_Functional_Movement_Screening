0a306ab2bf41c556f430860392a82e258dec465b
Mor Ariel <mor.ariel01@gmail.com>
2025-11-16 17:39:28 +0200

    Adding pages of camera calibration, practice and game

--- src/componentsClient/cameraCalibration.jsx
+++ src/componentsClient/cameraCalibration.jsx
@@ -1,9 +1,56 @@
-//import React from 'react'
+// import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import reactIcon from '../assets/react.svg';
+import React, { useRef, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
+
+
 
 function CameraCalibration() {
-  return (
-    <div>Camera Calibration</div>
-  )
-}
+  let nav = useNavigate();
+  const videoRef = useRef(null);
+  const location = useLocation();
+  const fromPage = location.state?.from
 
-export default CameraCalibration
+  const getVideo = () => {
+    navigator.mediaDevices
+      .getUserMedia({ video: {width: 1280, height: 720} })
+      .then(stream => {
+        let video = videoRef.current;
+        video.srcObject = stream;
+        video.play();
+    })
+    .catch(err => {
+      console.error(err);
+    })
+  }
+
+  useEffect(() => {
+    getVideo();
+  }, [videoRef]);
+
+
+
+  return (
+    <div>
+      <div className=" container mt-5 shadow-lg p-4 d-flex flex-column text-center" style={{ width: '80%', maxWidth: '500px', backgroundColor: 'white' }}>
+        <div className="row justify-content-center">
+          <img src={reactIcon} alt="React" style={{ width: '64px', height: '64px' }} />
+          <h4 className='m-2'>Camera Calibration</h4>
+          <video ref={videoRef} autoPlay style={{ width: '100%', border: '1px solid #ccc', borderRadius: '8px' }} >
+          </video>
+        </div>
+      </div>
+      <div>
+        <button onClick={() => nav("/" + fromPage) } style={{ width: '6%', maxWidth: '500px', backgroundColor: 'rgb(54, 227, 215)', bottom: '20px', right: '20px', position: 'fixed', borderColor: 'rgb(54, 227, 215)', borderRadius: '5px', padding: '10px', color: 'white', fontWeight: 'bold'}}>
+          Continue
+        </button>
+      </div>
+    </div>
+    
+  );
+};
+
+
+export default CameraCalibration;
--- src/componentsClient/practice.jsx
+++ src/componentsClient/practice.jsx
@@ -1,9 +1,67 @@
-//import React from 'react'
+// import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import reactIcon from '../assets/react.svg';
+import React, { useRef, useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+
+
+
 
 function Practice() {
-  return (
-    <div>Practice</div>
-  )
-}
+  let nav = useNavigate();
+  const videoRef = useRef(null);
+  const location = useLocation();
+  const fromPage = location.state?.from
+  const [time, setTime] = useState(5);
+  const [isRunning, setIsRunning] = useState(false);
+  const [showButton, setShowButton] = useState(true);
 
-export default Practice
+
+  const getVideo = () => {
+    navigator.mediaDevices
+      .getUserMedia({ video: {width: 1280, height: 720} })
+      .then(stream => {
+        let video = videoRef.current;
+        video.srcObject = stream;
+        video.play();
+    })
+    .catch(err => {
+      console.error(err);
+    })
+  }
+
+  useEffect(() => {
+    getVideo();
+  }, [videoRef]);
+
+  useEffect(() => {
+    let timer;
+    if (isRunning && time > 0) {
+      timer = setInterval(() => {
+        setTime(prev => prev - 1);
+      }, 1000);
+    }
+    return () => clearInterval(timer);
+  }, [isRunning, time]);
+
+  return (
+    <div>
+          <img src={reactIcon} alt="React" style={{ width: '64px', height: '64px' }} />
+          <div style={{ width: '70%', height: '70%', position: 'absolute', top: '50px', right: '50px', backgroundColor: '#2c2a2aff', border: '2px solid #ccc', borderRadius: '8px', padding: '20px', boxShadow: '0 4px 8px rgba(0,0,0,0.2)' }} >
+            <div>
+              <p style={{ color: 'white', fontSize: '18px' }}>Time left: {time} seconds</p>
+              {showButton && (
+                <button onClick={() => { setShowButton(false); setIsRunning(true); }} style={{ padding: '10px 20px', fontSize: '16px', borderRadius: '5px', border: 'none', backgroundColor: '#36e3d7',
+                                                                  color: 'white', fontWeight: 'bold', top: '50%', left: '50%', position: 'absolute', transform: 'translate(-50%, -50%)'}}>Play</button>
+              )}
+            </div>
+          </div>
+          <video ref={videoRef} style={{ width: '20%', height: 'auto', position: 'absolute', bottom: '50px', left: '50px', objectFit: 'cover' }} >
+          </video>
+    </div>
+    
+  );
+};
+
+
+export default Practice;
